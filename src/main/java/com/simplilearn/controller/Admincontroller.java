62bca50b2f226494e94b99904ad2dedf1acfb966
joseph <joseph@joseph>
2023-03-14 23:16:40 +0530

    4th commit

--- src/main/java/com/simplilearn/controller/Admincontroller.java
+++ src/main/java/com/simplilearn/controller/Admincontroller.java
@@ -1,12 +1,14 @@
 package com.simplilearn.controller;
 
 import java.io.File;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +23,7 @@ import com.simplilearn.model.Admin;
 import com.simplilearn.model.Medicine;
 import com.simplilearn.service.Adminservice;
 import com.simplilearn.service.Medicineservice;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class Admincontroller {
 	@Autowired
--- src/main/java/com/simplilearn/controller/Usercontroller.java
+++ src/main/java/com/simplilearn/controller/Usercontroller.java
@@ -3,6 +3,7 @@ package com.simplilearn.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.simplilearn.dto.RegisterUserDto;
 import com.simplilearn.model.Users;
 import com.simplilearn.service.Userservice;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class Usercontroller {
 
